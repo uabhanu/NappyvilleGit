b97d34151b2b2ad44f55b1e536a7596de336b3ee
Bhanu <uabhanu@gmail.com>
2017-08-20 13:59:34 +0100

    Section Lecture 133 Finished

---
+++ NappyvilleAndroid/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] float m_loadTime;
+
+    void Start()
+    {
+        Invoke("LoadNextLevel" , m_loadTime);    
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QuitButton Pressed");
+    }
+
+    public void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if(currentSceneIndex < 1)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+    }
+
+    public void LoadScene(string name)
+    {
+        Debug.Log("StartButton Pressed");
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ NappyvilleAndroid/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] AudioClip[] m_soundsArray;
+
+    AudioSource m_audioSource;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);   
+    }
+
+    void Start()
+    {
+        m_audioSource = GetComponent<AudioSource>();
+	}
+	
+    void OnLevelWasLoaded(int level)
+    {
+        AudioClip currentLevelMusic = m_soundsArray[level];
+        m_audioSource.clip = currentLevelMusic;
+        m_audioSource.loop = true;
+        m_audioSource.Play();
+    }
+}
