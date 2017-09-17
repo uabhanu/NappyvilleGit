20beacfc1e27b5e283cefed9bacb285a0166b625
Bhanu <uabhanu@gmail.com>
2017-09-17 17:05:28 +0100

    Section 7 Lecture 148 Finished

---
+++ NappyvilleAndroid/Assets/Scripts/Lizard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lizard : MonoBehaviour
+{
+    Animator m_animator;
+
+	void Start()
+    {
+	    m_animator = GetComponent<Animator>();
+        
+        StartCoroutine("AnimsRoutine");
+	}
+	
+	IEnumerator AnimsRoutine()
+    {
+        yield return new WaitForSeconds(3f);
+        m_animator.SetBool("Walk" , true);
+
+        yield return new WaitForSeconds(6f);
+        m_animator.SetBool("Attack" , true);
+
+        yield return new WaitForSeconds(3f);
+        m_animator.SetBool("Attack" , false);
+
+        StartCoroutine("AnimsRoutine");
+    }
+}
