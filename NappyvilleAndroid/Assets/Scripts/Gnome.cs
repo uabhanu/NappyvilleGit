80026b8ee0fad1ed77777b7b3cc3efb7665e8c67
Bhanu <uabhanu@gmail.com>
2017-09-17 20:57:52 +0100

    Section 7 Lecture 150 Finished

---
+++ NappyvilleAndroid/Assets/Scripts/Gnome.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gnome : MonoBehaviour
+{
+    Animator m_animator;
+
+	void Start()
+    {
+	    m_animator = GetComponent<Animator>();	
+        StartCoroutine("AnimsRoutine");
+	}
+	
+	IEnumerator AnimsRoutine()
+    {
+        yield return new WaitForSeconds(3f);
+        m_animator.SetBool("Hop" , true);
+
+        yield return new WaitForSeconds(3f);
+        m_animator.SetBool("Hop" , false);
+
+        StartCoroutine("AnimsRoutine");
+    }
+}
---
+++ NappyvilleAndroid/Assets/Scripts/Gravestone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravestone : MonoBehaviour
+{
+    Animator m_animator;
+
+	void Start()
+    {
+	    m_animator = GetComponent<Animator>();	
+	}
+	
+	void Attacked()
+    {
+
+    }
+
+    void BackToIdle()
+    {
+
+    }
+}
