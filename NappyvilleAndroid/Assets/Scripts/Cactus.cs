b48b88fc4b24925c94161cac11dff97e7260cc62
Bhanu <uabhanu@gmail.com>
2017-09-17 19:33:13 +0100

    Section 7 Lecture 149 Finished

---
+++ NappyvilleAndroid/Assets/Scripts/Cactus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactus : MonoBehaviour
+{
+	Animator m_animator;
+
+	void Start()
+    {
+	    m_animator = GetComponent<Animator>();	
+        StartCoroutine("AnimsRoutine");
+	}
+	
+	IEnumerator AnimsRoutine()
+    {
+        yield return new WaitForSeconds(4f);
+        m_animator.SetBool("Attack" , true);
+
+        yield return new WaitForSeconds(3f);
+        m_animator.SetBool("Attack" , false);
+
+        StartCoroutine("AnimsRoutine");
+    }
+}
