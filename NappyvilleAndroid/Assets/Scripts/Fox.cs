a9cc0121bbf16e422a68969de9fe62ce9c1d2544
Bhanu <uabhanu@gmail.com>
2017-09-21 20:08:32 +0100

    Section 7 Lecture 156 Finished

--- NappyvilleAndroid/Assets/Scripts/Fox.cs
+++ NappyvilleAndroid/Assets/Scripts/Fox.cs
@@ -5,27 +5,49 @@ using UnityEngine;
 public class Fox : MonoBehaviour
 {
     Animator m_animator;
+    bool m_isWalking;
+
+    [Range(0.0f , 1.5f)] [SerializeField] float m_walkSpeed;
 
 	void Start()
     {
 	    m_animator = GetComponent<Animator>();	
 	}
+
+    void Update()
+    {
+		if(Time.timeScale == 0)
+        {
+            return;
+        }
+
+        Walk();
+	}
 	
 	void Attack()
     {
+        m_isWalking = false;
         m_animator.SetBool("Attack" , true);
         m_animator.SetBool("Jump" , false);
     }
 
     void Jump()
     {
+        m_isWalking = false;
         m_animator.SetBool("Attack" , false);
         m_animator.SetBool("Jump" , true);
     }
 
     void Walk()
     {
+        m_isWalking = true;
         m_animator.SetBool("Attack" , false);
         m_animator.SetBool("Jump" , false);
+        m_animator.SetBool("Walk" , true);
+
+        if(m_isWalking)
+        {
+            transform.Translate(Vector2.left * m_walkSpeed * Time.deltaTime);
+        }
     }
 }
--- NappyvilleAndroid/Assets/Scripts/Lizard.cs
+++ NappyvilleAndroid/Assets/Scripts/Lizard.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class Lizard : MonoBehaviour
 {
     Animator m_animator;
+    bool m_isWalking;
+
+    [Range(0.0f , 1.5f)] [SerializeField] float m_walkSpeed;
+
 
 	void Start()
     {
 	    m_animator = GetComponent<Animator>();
         StartCoroutine("AnimsRoutine");
 	}
+
+    void Update()
+    {
+		if(Time.timeScale == 0)
+        {
+            return;
+        }
+
+        if(m_isWalking)
+        {
+            transform.Translate(Vector2.left * m_walkSpeed * Time.deltaTime);
+        }
+	}
 	
 	IEnumerator AnimsRoutine()
     {
         yield return new WaitForSeconds(3f);
+        m_isWalking = true;
         m_animator.SetBool("Walk" , true);
 
         yield return new WaitForSeconds(6f);
+        m_isWalking = false;
         m_animator.SetBool("Attack" , true);
 
         yield return new WaitForSeconds(3f);
