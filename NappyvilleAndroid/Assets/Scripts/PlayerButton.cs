6c2ea8e38d94fee1ff10ae91da1f41472a11b422
Bhanu <uabhanu@gmail.com>
2017-10-04 23:19:44 +0100

    Section 7 Lecture 164 WIP

---
+++ NappyvilleAndroid/Assets/Scripts/PlayerButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerButton : MonoBehaviour
+{
+    [SerializeField] PlayerButton[] m_playerButtons;
+
+	void Start()
+    {
+	    m_playerButtons = FindObjectsOfType<PlayerButton>();   
+	}
+	
+	public void SpriteColourToggle()
+    {
+        foreach(PlayerButton playerButton in m_playerButtons)
+        {
+            playerButton.GetComponent<Image>().color = Color.black;
+        }
+
+        GetComponent<Image>().color = Color.white;
+    }
+}
