d05ad0b6be1d50177aee10a7425dc35d5b2d77ef
Bhanu <uabhanu@gmail.com>
2017-10-11 20:19:21 +0100

    Use Raycast for Players to attack Section 7 Lecture 169 Up Next

---
+++ NappyvilleAndroid/Assets/Scripts/Star.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    BoxCollider2D m_starCollider2D;
+
+    //Star movement and disappearing logic coming soon
+
+	void Start()
+    {
+		m_starCollider2D = GetComponent<BoxCollider2D>();
+	}
+	
+	void Update()
+    {
+		
+	}
+}
