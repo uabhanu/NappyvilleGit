8d1b92afe1b69552ed73a75fdfaabd138e15a1dc
Bhanu <uabhanu@gmail.com>
2017-10-29 10:08:07 +0000

    Star logic modified slightly Last Level Star Spawn logic wIP

--- NappyvilleAndroid/Assets/Scripts/Star.cs
+++ NappyvilleAndroid/Assets/Scripts/Star.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Star : MonoBehaviour
 {
-    float m_fallSpeed = 1.5f;
+    float m_fallSpeed = 1.5f , m_destination = 0.02f;
 
     [SerializeField] PlayerButton m_playerButton;
 
@@ -29,7 +29,7 @@ public class Star : MonoBehaviour
             transform.localScale = new Vector2(transform.localScale.x + 0.01f , transform.localScale.y + 0.01f);
         }
 
-        if(transform.localScale.y >= 1 && transform.position.y > 1f)
+        if(transform.localScale.y >= 1 && transform.position.y > m_destination)
         {
             transform.Translate(-Vector2.up * m_fallSpeed * Time.deltaTime);
         }
@@ -39,8 +39,9 @@ public class Star : MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
 
-        if(transform.position.y <= 1f)
+        if(transform.position.y < m_destination)
         {
+            Debug.Log(transform.position);
             Destroy(gameObject);
         }
      
